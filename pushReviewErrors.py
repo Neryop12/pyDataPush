a0a1864d150f66eb84bfa68c438980e0f37f3465
SIPAProyecto <soporte.sistemas@cdag.com.gt>
2019-08-06 14:29:12 -0600

    errorPresupuesto
    con fecha acutal y fecha de un día antes

--- pushReviewErrors.py
+++ pushReviewErrors.py
@@ -11,7 +11,7 @@ import time
 def openConnection():
     global conn
     try:
-        conn = mysql.connect(host='3.95.117.169',database='MediaPlatforms',user='omgdev',password='Sdev@2002!',autocommit=True)
+        conn = conn = mysql.connect(host='localhost',database='mediaplatforms',user='root',password='1234',autocommit=True)
     except:
         print("ERROR: NO SE PUEDO ESTABLECER CONEXION MYSQL.")
         sys.exit()
