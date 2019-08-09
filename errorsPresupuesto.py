8784a7c9c235092f5511256874b13155b5d3370b
SIPAProyecto <soporte.sistemas@cdag.com.gt>
2019-08-09 12:22:23 -0600

    ErrorPresupuesto Produccion

--- errorsPresupuesto.py
+++ errorsPresupuesto.py
@@ -11,7 +11,7 @@ conn = None
 def openConnection():
     global conn
     try:
-        conn = mysql.connect(host='localhost',database='mfcgt',user='root',password='1234',autocommit=True)
+        conn = mysql.connect(host='3.95.117.169',database='MediaPlatforms',user='omgdev',password='Sdev@2002!',autocommit=True)
     except:
         logger.error("ERROR: NO SE PUEDO ESTABLECER CONEXION MYSQL.")
         sys.exit()
@@ -21,8 +21,11 @@ def openConnection():
 def ComparacionErrores(conn):
     cur=conn.cursor(buffered=True)
     Errores=[]
+    fechahoy = datetime.now() 
+    dayhoy = fechahoy.strftime("%Y-%m-%d")
     #Query para insertar los datos, Media  -> OC
-    sqlInserErrors = "INSERT INTO mediaplatforms.ErrorsCampaings(Error,Comentario,Media,TipoErrorID,CampaingID,Impressions,StatusCampaing) VALUES (%s,%s,%s,%s,%s,%s,%s)"
+    sqlInserErrors = "INSERT INTO MediaPlatforms.ErrorsCampaings(Error,Comentario,Media,TipoErrorID,CampaingID,Impressions,StatusCampaing,Estado) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)"
+    sqlCamping = "select CampaingID,Campaingname from MediaPlatforms.Campaings where EndDate > '{}' and (Campaignstatus='ACTIVE' or Campaignstatus='enabled') ;".format(str(dayhoy))
     try:
         print(datetime.now())
         #Query para seleccionar los datos de (orden de compra, presupuesto, flowid y el idcompra) se realizo dos inner joins en con los pk
@@ -34,78 +37,103 @@ def ComparacionErrores(conn):
         fechaayer = datetime.now() - timedelta(days=1)
         #Formato de las fechas para aceptar en el GET
         dayayer = fechaayer.strftime("%Y-%m-%d")
-        fechahoy = datetime.now() 
-        dayhoy = fechahoy.strftime("%Y-%m-%d")
+       
         data = mp.array(cur.fetchall())
+        cur.execute(sqlCamping)
+        camp = cur.fetchall()
         #Obttencion del GET con los datos de PBI
-        r=requests.get("http://10.10.2.99:10000/pbi/api_gt/public/api/v1/ordenes_fl/{}/{}".format(str(dayayer),str(dayhoy)))
+        r=requests.get("http://10.10.2.99:10000/pbi/api_gt/public/api/v1/ordenes_fl/2019-01-01/{}".format(str(dayhoy)))
         #Primero se convierte el request a JSON
         r=r.json()
         #Posteriormente se convierte a un array numpy
         ap=mp.array(r) 
         #Buqueda de la base de datos al API
-        for rowBase in data:
-            #Se declaran dos variables Booleanas, para saber si se encuentra el dato dentro del API existe->Presupuseto codigo->orden de compra
-            existe=False
-            codigo=False
-            #Busqueda de presupuesto
-            #Primero reviza si el presupuesto en la base de datos no esta vacio, es nullo o es 0
-            if not rowBase[1] or rowBase[1]=='0':
-                Error = 'Codigo de Presupuesto esta vacio o es 0.'
-                Comentario = 'El Codigo de presupuesto no esta asignado.'
-                nuevo=[Error,Comentario,'OC','7',rowBase[2],'0','ACTIVE']
-                Errores.append(nuevo)
-            #De no serlo se recorre el array del API, para buscar alguna considencia en el mismo. 
-            else:
-                for rowApi in ap:
-                    if(rowBase[1]==rowApi['codigo_presupuesto']):
-                        #De existir un presupuesto, reviza si en esa row se encuntra el mismo Flow_id que en la base de datos
-                        if(str(rowBase[3])==rowApi['flow_id']):
-                            existe=True
-                        else:
-                             Error = 'Flow id no encontrado.'
-                             Comentario = 'Error el Flow id  Ingresado no es mismo '
-                             nuevo=[Error,Comentario,'OC','7',rowBase[2],'0','ACTIVE']
-                             Errores.append(nuevo)
-                        break
-                #Si no existe se inserta un nuevo error
-                if not existe:
-                    Error = 'Codigo de Presupuesto No.' + str(rowBase[1]) + ' no encontrado.'
-                    Comentario = 'Error el codigo de Presupuesto Ingresado no se encuentra en '
-                    nuevo=[Error,Comentario,'OC','7',rowBase[2],'0','ACTIVE']
-                    Errores.append(nuevo)
-            #Busqueda de orden de compra
-            if not rowBase[0] or rowBase[0]=='0':
-                Error = 'Codigo de Orden esta vacio o es 0.'
-                Comentario = 'El Codigo de orden no esta asignado.'
-                nuevo=[Error,Comentario,'OC','8',rowBase[2],'0','ACTIVE']
-                Errores.append(nuevo)
-            else:
-                for rowApi in ap:
-                    if(str(rowBase[0])==rowApi['numero_orden']):
-                        if  rowBase[1]!=rowApi['codigo_presupuesto']:
-                            Error = 'Numero de Orden ' + str(rowBase[1]) + '.'
+        for result in camp:
+            #Variables bool para saber si se tubo una coencidencia en el ciclo for
+            #Variable para ODC
+            ODCb = False
+            #Variable para el Presupuesto
+            Presb = False
+            #Variable para el numero de orden de compra 
+            Codb = False
+            #Variable para el flow id
+            Flowb = False
+            #Nomenclatura del campaing name
+            Nomenclatura = result[1]
+            CampaingID = result[0]
+            searchObj = re.search(r'^(GT|CAM|RD|US|SV|HN|NI|CR|PA|RD|PN|CHI|HUE|PR)_([a-zA-ZáéíóúÁÉÍÓÚÑñ\s0-9-/.+&]+)_([a-zA-Z0-9-/.+&]+)_([a-zA-ZáéíóúÁÉÍÓÚÑñ0-9-/.+&]+)_([a-zA-ZáéíóúÁÉÍÓÚÑñ0-9-/.+&]+)_([a-zA-ZáéíóúÁÉÍÓÚÑñ0-9-/.+&]+)_([a-zA-Z-/.+]+)_([a-zA-ZáéíóúÁÉÍÓÚÑñ.+]+)_(ENE|FEB|MAR|ABR|MAY|JUN|JUL|AGO|SEP|OCT|NOV|DIC)_(19)_([0-9,.]+)_(BA|AL|TR|TRRS|IN|DES|RV|CO)_([0-9,.]+)_(CPM|CPMA|CPVi|CPC|CPI|CPD|CPV|CPCo|CPME|CPE|PF|RF|MC|CPCo)_([0-9.,]+)_([a-zA-Z-/áéíóúÁÉÍÓÚÑñ.+]+)_([a-zA-Z-/áéíóúÁÉÍÓÚÑñ.+]+)_([a-zA-Z-/áéíóúÁÉÍÓÚÑñ.+]+)_([0-9,.-]+)(_B-)?([0-9]+)?(_S-)?([0-9]+)?(\(([0-9.)]+)\))?', Nomenclatura, re.M | re.I)
+            if searchObj:
+                #Posicion 19 Numero de Orden << Como pueden ver más de una por campaña se agrrega a un arreglo
+                NomODCAR = searchObj.group(19)
+                #Se realiza un split - para obtener todos los nummeros de orden 
+                NomODC = NomODCAR.split('-')
+                #For para recorrer los numeros de orden
+                for Nom in NomODC:
+                    #Poscion 11 inversion de la campaña
+                    NomInversion = float(searchObj.group(11))
+                    #Si el numero de orden es menor a 1 quiere decir que no esta asignado
+                    if int(Nom) < 1:
+                        Error = 'Implementado sin orden de compra'
+                        Comentario = 'El numero de orden de compra no esta asignado.'
+                        nuevo=[Error,Comentario,'OC','8',CampaingID,'0','ACTIVE','1']
+                        Errores.append(nuevo)
+                    else:
+                        #De lo contrario se recorre el API de MTS para encontrar el numero de orden
+                        for rowApi in ap:
+                            if int(Nom) ==  int(rowApi['numero_orden']):
+                                #Reviza primero si el flow_id del API esta ingresado correctamente
+                                if not rowApi['flow_id'] or int(rowApi['flow_id'])==0:
+                                    Error = 'Error no existe Flow asignado'
+                                    Comentario = 'El flow id  no esta asignado.'
+                                    nuevo=[Error,Comentario,'OC','12',CampaingID,'0','ACTIVE','0']
+                                    Errores.append(nuevo)
+                                    ODCb = True
+                                    break
+                                else:
+                                    #De encontralo se realiza un For para buscar entre MediaFlowChar
+                                    for rowMFC in data:
+                                        Presb = False
+                                        Codb = False
+                                        #Se pregunta si el Flow id existe en MFC
+                                        if int(rowApi['flow_id']) == rowMFC[2]:
+                                            #De encontrase se reviza si el codigo de presupuesto es el mismo que en MTS
+                                            if str(rowApi['codigo_presupuesto']) == rowMFC[1]:
+                                                Presb = True
+                                            #De igual manera se pregunta para el numero de orden 
+                                            if int(rowApi['numero_orden']) == rowMFC[0]:
+                                                Codb = True
+                                            Flowb = True
+                                            break
+                                        #Insersiones aun no implementadas en la plataforma (Estado 0)
+                                        if not Flowb:
+                                            Error = 'Error flow ' + str(rowApi['flow_id']) + '.'
+                                            Comentario = 'Error el flow no se encontro'
+                                            nuevo=[Error,Comentario,'OC','12',CampaingID,'0','ACTIVE','0']
+                                            Errores.append(nuevo)
+                                        if not Presb:
+                                            Error = 'Error Presupuesto ' + str(rowApi['codigo_presupuesto']) + '.'
+                                            Comentario = 'Error el Presupuesto no se encontro'
+                                            nuevo=[Error,Comentario,'OC','12',CampaingID,'0','ACTIVE','0']
+                                            Errores.append(nuevo)
+                                        if not Codb:
+                                            Error = 'Error orden de compra ' + str(rowApi['numero_orden']) + '.'
+                                            Comentario = 'Error la orden de compra no se encontro'
+                                            nuevo=[Error,Comentario,'OC','12',CampaingID,'0','ACTIVE','0']
+                                            Errores.append(nuevo)
+                                        if Presb and Codb:
+                                            if NomInversion >= float(rowApi['valor_total']):
+                                                Error = 'Error de inversion ' 
+                                                Comentario = 'Error la inversion supera lo planificado'
+                                                nuevo=[Error,Comentario,'OC','12',CampaingID,'0','ACTIVE','0']
+                                                Errores.append(nuevo)
+                                    ODCb = True
+                                    break
+                        if not ODCb:
+                            Error = 'Numero de Orden ' + str(Nom) + '.'
                             Comentario = 'Error el numero de orden Ingresado  no esta asigando al mismo presupuesto'
-                            nuevo=[Error,Comentario,'OC','8',rowBase[2],'0','ACTIVE']
+                            nuevo=[Error,Comentario,'OC','8',CampaingID,'0','ACTIVE','1']
                             Errores.append(nuevo)
-                        if str(rowBase[3])!=rowApi['id_compra']:
-                            Error = 'Numero de Orden ' + str(rowBase[1]) + '.'
-                            Comentario = 'Error el numero de orden Ingresado  no esta asigando al mismo id de orden'
-                            nuevo=[Error,Comentario,'OC','8',rowBase[2],'0','ACTIVE']
-                            Errores.append(nuevo)
-                        else:
-                            codigo=True
-                            break
-                        break
-                if not codigo:
-                    Error = 'Numero de Orden ' + str(rowBase[1]) + ' no encontrado.'
-                    Comentario = 'Error el numero de orden Ingresado no se encuentra en '
-                    nuevo=[Error,Comentario,'OC','8',rowBase[2],'0','ACTIVE']
-                    Errores.append(nuevo)
-        
-                    
         cur.executemany(sqlInserErrors,Errores)
-
     except Exception as e:
         print(e)
     finally:
