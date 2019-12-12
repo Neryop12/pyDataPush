f76b9fca5dc1eadc0de60f6631bf9db651b46cee
josecardleon <jdeleon@omg.com.gt>
2019-12-12 09:09:48 -0600

    Commit AdMovil

--- post_resultados_adsmovil.py
+++ post_resultados_adsmovil.py
@@ -51,9 +51,21 @@ def pushAdsMovil(conn):
     cur=conn.cursor(buffered=True)
     accounts=[]
     campanas=[]
+    conjuntos=[]
+    cmetrics=[]
+    ametrics=[]
+    anuncios=[]
 
-    sqlInsertCampaing = "INSERT INTO CampaingsAM (`CampaingID`, `Campaingname`, `ad`, `Impressions`, `Clicks`, `Ctr`, `Video_firstquartile`, `Video_midpoint`, `Video_thirdquartile`, `Video_completed`, `cost`, `CPM`, `AccountsID`, `StartDate`)  VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s) ON DUPLICATE KEY UPDATE Campaingname=VALUES(Campaingname),ad=VALUES(ad), Impressions=VALUES(Impressions),Clicks=VALUES(Clicks),Ctr=VALUES(Ctr),Video_firstquartile=VALUES(Video_firstquartile),Video_midpoint=VALUES(Video_midpoint),Video_thirdquartile=VALUES(Video_thirdquartile),cost=VALUES(cost),CPM=VALUES(CPM) "
+    sqlInsertCampaing = "INSERT INTO Campaings (`CampaingID`, `Campaingname`, `AccountsID`,`Campaignstatus`)  VALUES (%s,%s,%s,'ACTIVE') ON DUPLICATE KEY UPDATE Campaingname=VALUES(Campaingname),StartDate=VALUES(StartDate)"
+
+    AdsMetrics = "INSERT INTO metricsads (`AdID`,`Adname`,`Impressions`, `Clicks`, `Videowatchesat75`,`Videowatchesat100`, `Ctr`, `Cpm`, `cost`)  VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+
+    sqlConjunto = "INSERT INTO adsets (`CampaingID`, `AdSetID`,`Adsetname`,`Status`)  VALUES (%s,%s,%s,'ACTIVE') ON DUPLICATE KEY UPDATE Adsetname=(Adsetname)"
+
+    sqlAnuncio="INSERT INTO ads (`AdSetID`,`AdID`,`Adname`,`Status`,`Media`) VALUES(%s,%s,%s,'ACTIVE','AM') ON DUPLICATE KEY UPDATE Adname=(Adname)"
+
     sqlInsertAccounts="INSERT INTO `MediaPlatforms`.`Accounts` (`AccountsID`, `Account`, `Media`, `State`) VALUES (%s, %s, %s, %s)ON DUPLICATE KEY UPDATE Account=VALUES(Account);"
+
     try:
         url='https://reportapi.adsmovil.com/api/campaign/details'
         Result2 = requests.get(
@@ -77,16 +89,30 @@ def pushAdsMovil(conn):
             searchObj = re.search(r'(GT|CAM|RD|US|SV|HN|NI|CR|PA|RD|PN|CHI|HUE|PR)_([a-zA-ZáéíóúÁÉÍÓÚÑñ\s0-9-/.+&]+)_([a-zA-Z0-9-/.+&]+)_([a-zA-ZáéíóúÁÉÍÓÚÑñ0-9-/.+&]+)', str(row[2]), re.M | re.I)
             if searchObj:
                 if searchObj.group(2)=='CLARO':
-                    AccountID=searchObj.group(0)+searchObj.group(1)+searchObj.group(2)
+                    AccountID=searchObj.group(1)+searchObj.group(2)+searchObj.group(3)+searchObj.group(4)
                 else:
-                    AccountID=searchObj.group(1)+searchObj.group(2)
+                    AccountID=searchObj.group(2)+searchObj.group(3)+searchObj.group(4)
+                adid=row[1]+row[3]
                 account=[AccountID,AccountID,'AM',1]
-                campana=[row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8],row[9],row[10],row[11],row[12],AccountID,row[0]]
+                campana=[row[1],row[2],AccountID]
+                conjunto=[row[1],AccountID,row[2]]
+                anuncio=[AccountID, adid,row[3]]
+                ametric=[adid,row[3],row[4],row[5],row[9],row[10],row[6],row[12],row[11]]
+
             accounts.append(account)
             campanas.append(campana)
+
+
+            conjuntos.append(conjunto)
+            anuncios.append(anuncio)
+            ametrics.append(ametric)
+
         cur.execute("SET FOREIGN_KEY_CHECKS=0")
-        cur.executemany(sqlInsertCampaing,campanas)
         cur.executemany(sqlInsertAccounts,accounts)
+        cur.executemany(sqlInsertCampaing,campanas)
+        cur.executemany(sqlConjunto,conjuntos)
+        cur.executemany(sqlAnuncio,anuncios)
+        cur.executemany(AdsMetrics,ametrics)
         cur.execute("SET FOREIGN_KEY_CHECKS=1")
         print('Success AdsMovil Campanas')
         fechahoy = datetime.now()
@@ -102,8 +128,21 @@ def pushAdsMovil(conn):
     finally:
         print (datetime.now())
 
+def resultsCamps(conn):
+    global cur
+    print (datetime.now())
+    cmetrics=[]
+    cur=conn.cursor(buffered=True)
+    sqlMetricsAds="SELECT  a.AdID,SUM(b.Clicks) as Clicks, SUM(b.Impressions) as Impressions, SUM(b.cost) as cost FROM ads a INNER JOIN metricsads b on a.AdID=b.AdID GROUP BY a.AdID"
+    sqlInsertMetrics=""
+    cur.execute(sqlMetricsAds,)
+    resultscon = cur.fetchall()
+    for row in resultscon:
+        cmetric=[row[0],row[1],row[2],row[3]]
+
 if __name__ == '__main__':
     openConnection()
     GetToken()
     pushAdsMovil(conn)
+    # resultsCamps(conn)
     conn.close()
