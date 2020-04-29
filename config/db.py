93c2454a8384d222edce12ad0fb96746e7b64246
josecardleon <jdeleon@omg.com.gt>
2020-04-29 09:44:40 -0600

    Version cambios

---
+++ config/db.py
@@ -0,0 +1,13 @@
+#Localhost
+DATABASE_CONFIG = {
+    'host': 'localhost',
+    'dbname': 'MediaPlatforms',
+    'user': 'omgdev',
+    'password': 'Sdev@2002!',
+    'port': 8889,
+    'autocommit': 'True'
+}
+
+#Produccion
+
+#Desarrollo
